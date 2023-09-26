324faca781a57a4213cb8decf04890e625297968
dnmd-joyeajin <dpwls3120@dnmd.com>
2023-09-26 17:15:08 +0900

    4챕터 완료

---
+++ src/components/Products.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+
+const Products = () => {
+  //   const [count, setCount] = useState(0);
+  const [products, setProducts] = useState([]);
+  const [checked, setChecked] = useState(false);
+  const handleChange = () => setChecked((prev) => !prev);
+
+  useEffect(() => {
+    fetch(`data/${checked ? 'sale_' : ''}products.json`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log('데이터를 받아옴');
+        setProducts(data);
+      });
+    return () => {
+      console.log('깨끗하게 청소하는 일을 함');
+    };
+  }, [checked]);
+
+  return (
+    <div>
+      <input
+        id="checkbox"
+        type="checkbox"
+        value={checked}
+        onChange={handleChange}
+      />
+      <label htmlFor="checkbox">Show Only Sale</label>
+      <ul>
+        {products.map((products) => (
+          <li key={products.id}>
+            <article>
+              <h3>{products.name}</h3>
+              <p>{products.price}</p>
+            </article>
+          </li>
+        ))}
+      </ul>
+      {/* <button onClick={() => setCount((prev) => prev + 1)}>{count}</button> */}
+    </div>
+  );
+};
+
+export default Products;
